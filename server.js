e4988f4495360e608eddea0993cb8dbe1ee1134d
Jian Jang <pureunnabi@gmail.com>
2024-05-16 19:43:59 +0900

    서버 생성

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const PORT = 3000;
+// const userRoutes = require('./routes/user_route');
+
+app.use(cors());
+app.use(bodyParser.json())
+// app.use('/user', userRoutes);
+
+// 서버 실행
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
